7a76018a41c97d599b1536b082d4058f966ca459
yong <1198749126@qq.com>
2019-10-14 01:20:17 -0700

    add config

--- TestDBConn/src/main/java/Main.java
+++ TestDBConn/src/main/java/Main.java
@@ -1,26 +1,44 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     //$ java -jar TestDBConn-1.0-SNAPSHOT-jar-with-dependencies.jar
 
-    private static final String url = "jdbc:mysql://30.23.9.255:3306/mlsc_xinfang_shenzhendb";
-    private static final String name = "com.mysql.cj.jdbc.Driver";
-    private static final String username = "dw_developer";
-    private static final String password = "dw_devRs#0";
+    private static String url = "";
+    private static String name = "";
+    private static String username = "";
+    private static String password = "";
+    private static String sql = "";
 
     private static Connection connection = null;
     private static Statement stmt = null;
 
     public static void main(String[] args) {
         try {
+            Map config = readConfig();
+            System.out.println("##$$##-->url-->" + config.get("url"));
+            System.out.println("##$$##-->name-->" + config.get("name"));
+            System.out.println("##$$##-->username-->" + config.get("username"));
+            System.out.println("##$$##-->password-->" + config.get("password"));
+            System.out.println("##$$##-->sql-->" + config.get("sql"));
+            url = config.get("url").toString();
+            name = config.get("name").toString();
+            username = config.get("username").toString();
+            password = config.get("password").toString();
+            sql = config.get("sql").toString();
+
             Class.forName(name);
             connection = DriverManager.getConnection(url, username, password);
             stmt = connection.createStatement();
-            String sql = "select count(*) as count_num from xf_complain";
             ResultSet rs = stmt.executeQuery(sql);
             rs.next();
             int count = rs.getInt("count_num");
@@ -31,6 +49,24 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Map<String, String> readConfig() throws Exception {
+        Map result = new HashMap();
+        File confFile = new File("config.txt");
+        System.out.println("##$$##-->reading from-->" + confFile.getAbsolutePath());
+        FileInputStream fileInputStream = new FileInputStream(confFile);
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String lineTxt = null;
+        while ((lineTxt = bufferedReader.readLine()) != null) {
+            String[] kv = lineTxt.split("-->");
+            result.put(kv[0].trim(), kv[1].trim());
+        }
+        bufferedReader.close();
+        inputStreamReader.close();
+        fileInputStream.close();
+        return result;
     }
 
 }
